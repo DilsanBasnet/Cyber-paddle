9ec8e28f03f284c6025d0eb862883df7b0a1ca0d
Dilsan Basnet <officialdilsanacc@gmail.com>
2026-06-02 14:35:04 +0545

    ball movement script added: random value for start & bounch angle, speed, and also bouncyness added to ball,

---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    public float speed =199f;
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        startforce();
+    }
+    private void startforce()
+    {
+        float x = Random.value < 0.5f ? -1.0f : 1.0f;
+        float y = Random.value < 0.5f ? Random.Range(-1.0f, -0.5f) 
+        : Random.Range(0.5f, 1.0f);
+
+    Vector2 direction = new Vector2(x,y);
+_rigidbody.AddForce(direction * this.speed);
+    }
+
+}
