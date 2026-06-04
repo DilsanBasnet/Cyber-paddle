e3b53be33de1870dc346346474bdf4d11aa8a587
Dilsan Basnet <officialdilsanacc@gmail.com>
2026-06-04 07:55:11 +0545

    gameoverpanel added, In which Player or Bot win Script added in gamemanager.cs and  mainmenu button, GameExit button added, not turn to give it awesome Ui

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Gamemanager : MonoBehaviour
 {
   private int _playerScore;
   private int _computerScore;
+
   public ball Ball;
   public TextMeshProUGUI playerScore;
   public TextMeshProUGUI computerScore;
   public paddles playerpaddle;
   public paddles computerpaddle;
+
+  public GameObject gameOverPanel;
+  public TextMeshProUGUI winner;
 
 
   public void TotalPlayerScore()
@@ -18,16 +23,51 @@ public class Gamemanager : MonoBehaviour
         _playerScore++;
         this.playerScore.text = _playerScore.ToString();
         Debug.Log(_playerScore);
-      ResetEverything(); 
-        
+        if(_playerScore >= 10)
+    {
+      DeclareWinner("PLAYER WIN");
     }
+    else
+    {
+       ResetEverything(); 
+    }
+      
+    }
+
     public void TotalComputerScore()
     {
         _computerScore++;
         this.computerScore.text = _computerScore.ToString();
         Debug.Log(_computerScore);
-        ResetEverything();
+        if(_computerScore >= 10)
+    {
+      DeclareWinner("BOT WIN");
+
     }
+     else {
+        ResetEverything();
+        }
+    }
+
+   private void DeclareWinner(string winnerMsg)
+  {
+    gameOverPanel.SetActive(true);
+    winner.text = winnerMsg;
+    Time.timeScale = 0f;
+  }
+
+  public void GoToMainMenu()
+  {
+    Time.timeScale = 1f;
+    SceneManager.LoadScene("MainMenuScene");
+  }
+
+  public void CloseGame()
+  {
+    Debug.Log("Exit Game");
+    Application.Quit();
+
+  }
     private void ResetEverything()
     {
         this.playerpaddle.ResetPosition();
